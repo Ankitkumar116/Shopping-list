d9cad2f85b49e81be644bd7f8b9dba220dc3ce69
Ankit Kumar <pekka0555@gmail.com>
2023-06-23 22:59:33 +0530

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,14 +1,20 @@
-var v = document.querySelector("input");
+let v = document.querySelector("input");
 let b = document.querySelector("button");
+let c =document.getElementById("rem");
+let flag=0;
 b.addEventListener('click',function(event){
-
   if(v.value!=""){
     let li=document.createElement("li");
     let div=document.createElement("div");
     let g=document.createTextNode(v.value);
     li.appendChild(g);
     document.querySelector("ul").appendChild(div).appendChild(li);
-    v.value="";
+    v.value=""; flag++;
   }
-
+});
+c.addEventListener('click', function(e){
+ if(flag!=0){
+  document.querySelector("ul").lastChild.remove();
+  flag--;
+ }
 });
